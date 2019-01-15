08896e72057c1b51344dce5e360ddc7e1c2077aa
Andrea Williams <anw0514@gmail.com>
2019-01-15 00:17:57 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,9 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-("Mr. Chocolate", 12, "female", "purple", "calm", 1),
-("Rowdy", 4, "male", "orange", "hungry", 1),
-("Tabitha", 5, "male", "blue", "blue", 1),
-("Sergeant Brown", 6, "female", "purple", "anxious", 1),
-("Melissa", 47, "male", "blue", "sleepy", 1),
-("Grinch", 31, "female", "green", "jolly", 1),
-("Wendy", 16, "male", "orange", "hungry", 1),
-(NULL, 78, "male", "brown", "regretful", 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+(1, "Mr. Chocolate", 12, "female", "purple", "calm", 1),
+(2, "Rowdy", 4, "male", "orange", "hungry", 1),
+(3, "Tabitha", 5, "male", "blue", "blue", 1),
+(4, "Sergeant Brown", 6, "female", "purple", "anxious", 1),
+(5, "Melissa", 47, "male", "blue", "sleepy", 1),
+(6, "Grinch", 31, "female", "green", "jolly", 1),
+(7, "Wendy", 16, "male", "orange", "hungry", 1),
+(8, NULL, 78, "male", "brown", "regretful", 0);
