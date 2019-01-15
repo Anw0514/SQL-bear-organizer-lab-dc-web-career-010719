ba9801dbfa9f98dcbd646c8fe4d5ce3fa302c29e
Andrea Williams <anw0514@gmail.com>
2019-01-15 00:17:07 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,9 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ("Mr. Chocolate", 12, "female", "purple", "calm", 1);
-VALUES ("Rowdy", 4, "male", "orange", "hungry", 1);
-VALUES ("Tabitha", 5, "male", "blue", "blue", 1);
-VALUES ("Sergeant Brown", 6, "female", "purple", "anxious", 1);
-VALUES ("Melissa", 47, "male", "blue", "sleepy", 1);
-VALUES ("Grinch", 31, "female", "green", "jolly", 1);
-VALUES ("Wendy", 16, "male", "orange", "hungry", 1);
-VALUES (NULL, 78, "male", "brown", "regretful", 0);
+("Mr. Chocolate", 12, "female", "purple", "calm", 1),
+("Rowdy", 4, "male", "orange", "hungry", 1),
+("Tabitha", 5, "male", "blue", "blue", 1),
+("Sergeant Brown", 6, "female", "purple", "anxious", 1),
+("Melissa", 47, "male", "blue", "sleepy", 1),
+("Grinch", 31, "female", "green", "jolly", 1),
+("Wendy", 16, "male", "orange", "hungry", 1),
+(NULL, 78, "male", "brown", "regretful", 0);
